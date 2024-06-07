8939abd971ff63b8840a11cee4f46d1d34b97b1e
Prathiksha A <139552246+prathiksha3@users.noreply.github.com>
2024-06-08 05:28:01 +0530

    Create lab1.py

---
+++ ML/lab1.py
@@ -0,0 +1,32 @@
+
+import pandas as pd
+df = pd.read_csv('EnjoySport.csv')
+#df = df.drop(['slno'],axis=1)
+column_length = df.shape[1]
+df.head()
+
+h = ['0']*(column_length-1)
+hp =[]
+hn =[]
+
+
+for training_example in df.values:
+    if training_example[-1] != 'no':
+        hp.append(list(training_example))
+    else:
+        hn.append(list(training_example))   
+
+
+
+for i in range(len(hp)):
+    for j in range(column_length - 1):
+        if (h[j]=="0"):
+            h[j]=hp[i][j]
+        if (h[j]!=hp[i][j]):
+            h[j]='?'
+        else:
+            h[j]=hp[i][j]
+
+print(f'Postive hypothesis:\n{hp}')
+print(f'Negative hypothesis:\n{hn}')
+print(f'Maximlly Specific hypothesis:\n{h}')
