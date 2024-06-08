87d456ef1857d4fedfd366ca920950616e64a791
Prathiksha A <139552246+prathiksha3@users.noreply.github.com>
2024-06-08 05:37:20 +0530

    Create 4 scheduling SJF.c

---
+++ Operating_system/4 scheduling SJF.c
@@ -0,0 +1,48 @@
+
+#include <stdio.h>
+int main()
+{
+    int time, burst_time[10],at[10],sum_burst_time=0,smallest,n,i;
+    int sumt=0,sumw=0;
+    printf("Enter the no of processes:\n");
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        printf("the arrival time for process P%d:",i+1);
+        scanf("%d",&at[i]);
+         printf("the burst time for process P%d:",i+1);
+        scanf("%d",&burst_time[i]);
+        sum_burst_time+=burst_time[i];
+    }
+    burst_time[9]=9999;
+    for(time=0;time<sum_burst_time;)
+    {
+        smallest=9;
+        for(i=0;i<n;i++)
+        {
+            if(at[i]<=time&&burst_time[i]&&burst_time[i]<burst_time[smallest])
+            smallest=i;
+        }
+        printf("P[%d]\t|\t%d\t|\t%d\n",smallest+1,time+burst_time[smallest]-at[smallest],time-at[smallest]);
+        sumt+=time+burst_time[smallest]-at[smallest];
+        sumw+=time-at[smallest];
+        time+=burst_time[smallest];
+        burst_time[smallest]=0;
+    }
+    printf("\n\n average waiting time=%f", sumw*1.0/n);
+    printf("\n\n average turnaround time=%f", sumt*1.0/n);
+    return 0;
+}
+
+/*4
+0 7
+2 4
+4 1
+5 4
+p[1]| 7| 0 |
+p[2]| 4| 3 |
+p[3]| 10| 6 |
+p[4]| 11| 7 |
+avg wt=4.000000
+avg tat=8.000000
+*/ 
