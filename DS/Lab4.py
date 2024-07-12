fdd652f581e033e6d9c112df0ad54688f42d266e
Prathiksha A <139552246+prathiksha3@users.noreply.github.com>
2024-07-12 08:15:49 +0530

    Update Lab4.py

--- DS/Lab4.py
+++ DS/Lab4.py
@@ -1,38 +1,39 @@
-
 import numpy as np
-from tqdm import tqdm
-x=np.array(([2,9],[1,5],[3,6]),dtype=float)
-y=np.array(([92],[86],[89]),dtype=float)
-x=x/np.amax(x,axis=0)
-y=y/100
-def sigmoid(x):
-    return 1/(1+np.exp(-x))
-def derivative(x):
-    return x*(1-x)
-epoch=5000
-lr=10
-input_neurons=2
-hidden_neurons=3
-output_neurons=1
-wh=np.random.uniform(size=(input_neurons,hidden_neurons))
-bh=np.random.uniform(size=(1,hidden_neurons))
-wout=np.random.uniform(size=(hidden_neurons,output_neurons))
-bout=np.random.uniform(size=(1,output_neurons))
-for i in range(epoch):
-    hinpl=np.dot(x,wh)
-    hinp=hinpl+bh
-    hlayer_act=sigmoid(hinp)
-    outinpl=np.dot(hlayer_act,wout)
-    outinp=outinpl+bout
-    output=sigmoid(outinp)
-    E0=y-output
-    outgrad=derivative(output)
-    d_output=E0*outgrad
-    EH=d_output.dot(wout.T)
-    hiddengrad=derivative(hlayer_act)
-    d_hiddenlayer=EH*hiddengrad
-    wout+=hlayer_act.T.dot(d_output)*lr
-    wh+=x.T.dot(d_hiddenlayer)*lr
-print("Input: \n"+str(x))
-print("Actual Output: \n"+str(y))
-print("Predicted output: \n",output)
+
+# Data and normalization
+x = np.array([[2, 9], [1, 9], [3, 6]], dtype=float) / np.array([3, 9])
+y = np.array([[92], [86], [89]], dtype=float) / 100
+
+# Activation functions
+sigmoid = lambda x: 1 / (1 + np.exp(-x))
+dsigmoid = lambda x: x * (1 - x)
+
+# Parameters
+epochs, lr = 5000, 0.1
+input_neurons, hidden_neurons, output_neurons = 2, 3, 1
+
+# Weights and biases
+wb = np.random.uniform(size=(input_neurons, hidden_neurons))
+bb = np.random.uniform(size=(1, hidden_neurons))
+wout = np.random.uniform(size=(hidden_neurons, output_neurons))
+bout = np.random.uniform(size=(1, output_neurons))
+
+# Training
+for _ in range(epochs):
+    # Forward Propagation
+    hlayer = sigmoid(np.dot(x, wb) + bb)
+    output = sigmoid(np.dot(hlayer, wout) + bout)
+    
+    # Backpropagation
+    error = y - output
+    d_output = error * dsigmoid(output)
+    d_hidden = d_output.dot(wout.T) * dsigmoid(hlayer)
+    
+    # Update weights and biases
+    wout += hlayer.T.dot(d_output) * lr
+    wb += x.T.dot(d_hidden) * lr
+
+# Results
+print("Input:\n", x)
+print("Actual:\n", y)
+print("Predicted:\n", output)
